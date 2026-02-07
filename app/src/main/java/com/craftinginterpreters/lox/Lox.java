78aff398f4565bfcfe3a03bb28a1891593fba8df
Chris Jurgens <chrisjurgens12317@gmail.com>
2026-02-07 15:09:50 -0500

    Add RuntimeError functionality, Hookup Interpreter in Lox.java, Format document files

--- app/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ app/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -30,6 +33,9 @@ public class Lox {
         //Indicate an error in the exit code
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -52,17 +58,25 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-       Parser parser = new Parser(tokens);
+        Parser parser = new Parser(tokens);
         Expr expression = parser.parse();
 
         // Stop if there was a syntax error.
-        if (hadError) return;
+        if (hadError) {
+            return;
+        }
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage()
+                + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where,
@@ -73,10 +87,10 @@ public class Lox {
     }
 
     static void error(Token token, String message) {
-    if (token.type == TokenType.EOF) {
-      report(token.line, " at end", message);
-    } else {
-      report(token.line, " at '" + token.lexeme + "'", message);
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
-  }
 }
